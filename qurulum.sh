2e332657a88e76cb1d7b2d81c71f16a5cd055b31
sekret666 <66795778+sekret666@users.noreply.github.com>
2021-09-03 21:49:03 +0400

    Create qurulum.sh

---
+++ qurulum.sh
@@ -0,0 +1,7 @@
+pkg install python
+pkg install python3
+pkg update
+git clone https://github.com/brendsupport/brend_installler
+cd brend_installler
+pip install -r requirements.txt
+python3 -m brend_installer
