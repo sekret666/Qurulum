5704fea14a23f6516715f7bb108fd0e01629b5b7
sekret666 <66795778+sekret666@users.noreply.github.com>
2021-09-03 23:29:58 +0400

    Update qurulum.sh

--- qurulum.sh
+++ qurulum.sh
@@ -1,8 +1,12 @@
 pkg install python
+clear
 pkg install python3
+clear
 pkg update
+clear
 git clone https://github.com/brendsupport/brend_installler
+clear
 cd brend_installler
+clear
 pip install -r requirements.txt
-python3 -m brend_installer
-exit
+python3 -m brend_installer && exit
